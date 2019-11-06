0ac6d4136ee28574ed05bd4619c5eb70d9b92df0
Martin Emil <mar10martinemil@gmail.com>
2019-11-06 03:37:20 +0200

    Initial commit

---
+++ src/com/martinemil/aast/sic/Main.java
@@ -0,0 +1,8 @@
+package com.martinemil.aast.sic;
+
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/com/martinemil/aast/sic/SicInstruction.java
@@ -0,0 +1,10 @@
+package com.martinemil.aast.sic;
+
+public class SicInstruction {
+
+    String label;
+    String instruction;
+    String operand;
+    String operand2;
+
+}
